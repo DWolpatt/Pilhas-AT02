6197b3331c5c190e63966790ae9e57fe0270aba3
DWolpatt <deborawolpatt@gmail.com>
2021-09-30 20:32:09 -0300

    Pilha Exemplo

---
+++ Pilha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Chapas = new Stack<string>(); //Empilhamento de chapas metálicas em rede fabril.
+
+            Chapas.Push("Chapa 1"); //Metodo que insere elementos na pilha.
+            Chapas.Push("Chapa 2");
+            Chapas.Push("Chapa 3");
+            Chapas.Push("Chapa 4");
+            Chapas.Push("Chapa 5");
+
+
+            foreach(string Chapa in Chapas) //percorrendo lista
+            {
+                Console.WriteLine(Chapa);
+
+            }
+
+
+            Console.WriteLine(Chapas.Pop()); //removendo item da pilha
+
+
+            foreach (string Chapa in Chapas) //percorrendo lista novamente
+            {
+                Console.WriteLine(Chapa);
+
+            }
+
+        }
+    }
+}
